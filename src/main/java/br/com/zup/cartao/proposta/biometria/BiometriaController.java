14331563a7c17d59222230c58c5aac937795543b
Lucas Mezuraro <lucas.mezuraro@zup.com.br>
2020-12-04 11:58:14 -0300

    adicionando logs ao processo de biometria

--- src/main/java/br/com/zup/cartao/proposta/biometria/BiometriaController.java
+++ src/main/java/br/com/zup/cartao/proposta/biometria/BiometriaController.java
@@ -1,7 +1,10 @@
 package br.com.zup.cartao.proposta.biometria;
 
 import br.com.zup.cartao.proposta.proposta.Proposta;
+import br.com.zup.cartao.proposta.proposta.PropostaController;
 import br.com.zup.cartao.proposta.proposta.PropostaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,19 +26,32 @@ public class BiometriaController {
     @Autowired
     private PropostaRepository propostaRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(BiometriaController.class);
+
     @PostMapping("/cartao/{id}")
     @Transactional
     public ResponseEntity<?> cadastrarBiometria(@PathVariable("id") String idCartao, @Valid @RequestParam MultipartFile fingerprint) throws IOException {
 
+        logger.info("iniciando processo de vinculacao de biometria ao cartão...");
+
         Proposta proposta = propostaRepository.findByCartaoNumeroCartao(idCartao).orElseThrow();
+
+        logger.info("cartão encontrado, checando existencia de outras biometrias associadas...");
+
         Biometria biometria = new Biometria(idCartao, fingerprint.getBytes(), fingerprint.getContentType(), fingerprint.getSize(), fingerprint.getOriginalFilename());
-        List<Biometria> biometriaList = new ArrayList<>();
+        List<Biometria> biometrias = new ArrayList<>();
+
+        logger.info("processando vinculação...");
+
         if (!proposta.getCartao().getBiometria().isEmpty()) {
-            biometriaList.addAll(proposta.getCartao().getBiometria());
+            biometrias.addAll(proposta.getCartao().getBiometria());
         }
-        biometriaList.add(biometria);
-        proposta.getCartao().setBiometria(biometriaList);
+
+        biometrias.add(biometria);
+        proposta.getCartao().setBiometria(biometrias);
         propostaRepository.save(proposta);
+
+        logger.info("biometria salva com sucesso!");
         return ResponseEntity.ok(proposta);
     }
 }
